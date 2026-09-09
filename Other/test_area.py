d0c70f9cf3a53d385c050510fee613a85774c1fa
rishi.khandelwal@fractal.ai <rishi.khandelwal@fractal.ai>
2026-09-09 13:27:52 +0000

    changes

--- Other/test_area.py
+++ Other/test_area.py
@@ -3,15 +3,15 @@ from circle_area import circlearea
 
 
 def test_circle_area():
-    assert circlearea(0) == 0
-    assert circlearea(1) == 3.14    
-    assert circlearea(2) == 12.56
-    assert circlearea(3) == 28.26
-    assert circlearea(5) == 78.5
-    assert circlearea(10) == 314.0
-    assert circlearea(100) == 31400.0
-    assert circlearea(50) == 7850.0
-    assert circlearea(25) == 1963.5
-    assert circlearea(12.5) == 490.875
-    assert circlearea(7.5) == 176.625
-    assert circlearea(15) == 706.5
+    assert circlearea(0) == pytest.approx(0)
+    assert circlearea(1) == pytest.approx(3.14)    
+    assert circlearea(2) == pytest.approx(12.56)
+    assert circlearea(3) == pytest.approx(28.26)
+    assert circlearea(5) == pytest.approx(78.5)
+    assert circlearea(10) == pytest.approx(314.0)
+    assert circlearea(100) == pytest.approx(31400.0)
+    assert circlearea(50) == pytest.approx(7850.0)
+    assert circlearea(25) == pytest.approx(1963.5)
+    assert circlearea(12.5) == pytest.approx(490.875)
+    assert circlearea(7.5) == pytest.approx(176.625)
+    assert circlearea(15) == pytest.approx(706.5)
